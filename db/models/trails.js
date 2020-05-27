59b3c794a6d90e3f4774f25eda1bf047cdbb13f5
Rob Visentin <jvisenti@gmail.com>
2020-05-28 01:37:43 +0200

    Added trails model and service

---
+++ db/models/trails.js
@@ -0,0 +1,37 @@
+const BaseService = require('../common/service.js');
+const knex = require('../knex.js');
+const knexPostgis = require("knex-postgis");
+const geoHash = require('../../app/lib/geoHash');
+
+const st = knexPostgis(knex);
+
+class Service extends BaseService {
+
+  async insertPoints(points, caseId) {
+    if (!points) throw new Error('Points are invalid');
+    if (!caseId) throw new Error('Case ID is invalid');
+
+    const pointRecords = [];
+
+    for(let point of points) {
+      let record = {};
+      let hash = await geoHash.encrypt(point);
+
+      if (hash) {
+        record.hash = hash.encodedString;
+        record.coordinates = st.setSRID(
+          st.makePoint(point.longitude, point.latitude),
+          4326
+        );
+        record.time = new Date(point.time * 1000); // Assumes time in epoch seconds
+        record.case_id = caseId;
+        pointRecords.push(record);
+      }
+    }
+
+    return knex(this._name).insert(pointRecords).returning('*');
+  }
+
+}
+
+module.exports = new Service('trails');
