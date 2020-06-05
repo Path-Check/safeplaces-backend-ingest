3de8401de3ade940c1115b4affd86bbaf994729f
Rob Visentin <jvisenti@gmail.com>
2020-06-05 17:19:43 +0200

    Fixed access code validation response format for non-existent codes

--- test/integration/accessCode.test.js
+++ test/integration/accessCode.test.js
@@ -26,7 +26,7 @@ describe('GET /access-code/valid', () => {
     result.should.have.status(400);
   });
 
-  it('should succeed when token is valid', async () => {
+  it('should succeed when code is valid', async () => {
     const result = await chai
       .request(server.app)
       .get('/access-code/valid')
@@ -34,10 +34,11 @@ describe('GET /access-code/valid', () => {
         accessCode: currentAccessCode.value,
       });
     result.should.have.status(200);
+    result.body.should.have.property('valid');
     result.body.valid.should.be.true;
   });
 
-  it('should succeed when token is invalid', async () => {
+  it('should succeed when code is invalid', async () => {
     await accessCodes.invalidate(currentAccessCode);
 
     const result = await chai
@@ -47,6 +48,19 @@ describe('GET /access-code/valid', () => {
         accessCode: currentAccessCode.value,
       });
     result.should.have.status(200);
+    result.body.should.have.property('valid');
+    result.body.valid.should.be.false;
+  });
+
+  it('should succeed when code does not exist', async () => {
+    const result = await chai
+      .request(server.app)
+      .get('/access-code/valid')
+      .send({
+        accessCode: "fake_code",
+      });
+    result.should.have.status(200);
+    result.body.should.have.property('valid');
     result.body.valid.should.be.false;
   });
 });
