e650870b1c635018aea951c14cc6d0e26392c01d
Daniel McCoy <dan.mccoy@rocketinsights.com>
2020-06-11 17:17:25 -0400

    remove access code seeds since they are no longer needed

--- db/seeds/development/access_codes_seed.js
+++
@@ -1,9 +0,0 @@
-exports.seed = function (knex) {
-  return knex('access_codes')
-    .del()
-    .then(async function () {
-      return knex('access_codes').insert({
-        value: '123456',
-      });
-    });
-};
